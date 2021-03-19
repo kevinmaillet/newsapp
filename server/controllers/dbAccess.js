43a0c79fc508d0ff2d6a74c06e61c880dba72acf
Kevin Maillet <kevinrmaillet@gmail.com>
2021-03-19 14:00:00 -0400

    updated route

--- server/controllers/dbAccess.js
+++ server/controllers/dbAccess.js
@@ -1,55 +1,65 @@
 const Article = require('../models/article.model');
 
 const updateDB = (category, articles = []) => {
-  //Map over api response and update mongoDB
-  for (let i = 0; i < articles.length; i++) {
-    Article.findOne({ title: articles[i].title }, (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      //Check if there is already an article with the same title
-      if (result) {
-        console.log('this is a duplicate');
-      } else {
-        let article = new Article({
-          category: category,
-          title: articles[i].title,
-          description: articles[i].description,
-          content: articles[i].content,
-          url: articles[i].url,
-          image: articles[i].image,
-          publishedAt: articles[i].publishedAt,
-          source: {
-            name: articles[i].source.name,
-            url: articles[i].source.url,
-          },
-        });
+  try {
+    //Map over api response and update mongoDB
+    for (let i = 0; i < articles.length; i++) {
+      Article.findOne({ title: articles[i].title }, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        //Check if there is already an article with the same title
+        if (result) {
+          console.log('this is a duplicate');
+        } else {
+          let article = new Article({
+            category: category,
+            title: articles[i].title,
+            description: articles[i].description,
+            content: articles[i].content,
+            url: articles[i].url,
+            image: articles[i].image,
+            publishedAt: articles[i].publishedAt,
+            source: {
+              name: articles[i].source.name,
+              url: articles[i].source.url,
+            },
+          });
 
-        article.save(function (err, doc) {
-          if (err) return console.error(err);
-          console.log('Document inserted successfully!');
-        });
-      }
-    });
+          article.save(function (err, doc) {
+            if (err) return console.error(err);
+            console.log('Document inserted successfully!');
+          });
+        }
+      });
+    }
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    return true;
   }
 };
 
 const fetchDB = async (category) => {
-  //Get articles by category
-  const data = await Article.find(
-    {
-      $or: [
-        { category: category },
-        { content: { $regex: category, $options: 'i' } },
-      ],
-    },
-    (err, result) => {
-      if (err) {
-        console.log(err);
+  try {
+    //Get articles by category
+    const data = await Article.find(
+      {
+        $or: [
+          { category: category },
+          { content: { $regex: category, $options: 'i' } },
+        ],
+      },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
       }
-    }
-  );
-  return data;
+    );
+    return data;
+  } catch (e) {
+    return [];
+  }
 };
 
 exports.updateDB = updateDB;
