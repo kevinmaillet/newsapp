20fa4cc40cc45389634833e2d6feada44ee06e13
Kevin Maillet <kevinrmaillet@gmail.com>
2021-03-24 15:25:35 -0400

    updated api calls

--- server/api/api.js
+++ server/api/api.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 
 const getTopNewsArticles = async () => {
   try {
-    const response = await axios.get(
-      `https://gnews.io/api/v4/top-headlines?country=us&token=db25ec16c61febb76f766b272bf90a25`
-    );
-    return new Promise((resolve, reject) => resolve(response.data.articles));
+    return await axios
+      .get(
+        `https://gnews.io/api/v4/top-headlines?country=us&token=db25ec16c61febb76f766b272bf90a25`
+      )
+      .then((response) => response.data.articles);
   } catch (err) {
     console.log("can't reach api");
   }
@@ -13,11 +14,11 @@ const getTopNewsArticles = async () => {
 
 const getArticlesByCategory = async (category) => {
   try {
-    const response = await axios.get(
-      `https://gnews.io/api/v4/search?q=${category}&country=us&token=db25ec16c61febb76f766b272bf90a25`
-    );
-
-    return new Promise((resolve, reject) => resolve(response.data.articles));
+    return await axios
+      .get(
+        `https://gnews.io/api/v4/search?q=${category}&country=us&token=db25ec16c61febb76f766b272bf90a25`
+      )
+      .then((response) => response.data.articles);
   } catch (err) {
     console.log("can't reach api");
   }
