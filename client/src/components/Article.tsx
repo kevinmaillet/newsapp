8d735bfced138a03b6faff706d4ebbbb20d26a6d
kevinmaillet <48228696+kevinmaillet@users.noreply.github.com>
2021-03-30 19:58:30 -0400

    Update Article.tsx

--- client/src/components/Article.tsx
+++ client/src/components/Article.tsx
@@ -15,7 +15,7 @@ const Article: React.FC<ArticleType> = (props) => {
         <div className="article__text">
           <h4 className="article__date">{moment(d).fromNow()}</h4>
           <h4 className="article__title">
-            {props.title} - {props.source.name}
+<!--             {props.title} - {props.source.name} -->
           </h4>
         </div>
       </a>
