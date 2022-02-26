1fdff9fe128a09ef4e8380dfed0642f5118314a7
kevin maillet <kevinmaillet@kevins-MacBook-Pro.local>
2022-02-25 19:08:25 -0500

    added rate limiter middleare

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dbConnect = require('./controllers/dbConnect');
 const cronJob = require('./services/cronJob');
 const helmet = require('helmet');
+const limiter = require('./middlewares/rateLimiter');
 const categoryRoute = require('./routes/categoryRoute');
 
 dbConnect();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(limiter);
 
 //Runs everyday at 9am, queries news api to update database
 cronJob();
