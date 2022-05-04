e7b1d31df03b4901a93dcb2a817eb530ddaadec7
kevin maillet <kevinrmaillet@gmail.com>
2022-05-03 20:30:05 -0400

    updated redis cache to store most recent articles

--- server/controllers/getPosts.js
+++ server/controllers/getPosts.js
@@ -7,6 +7,7 @@ const getPosts = async (req, res) => {
   const limit = req.body.limit;
   const skip = req.body.skip;
   const client = await redisClient();
+  const cache = await client.get(category);
 
   if (!req.headers.key === process.env.API_KEY) {
     res.send({ error: 'Wrong Api Key' });
@@ -15,36 +16,46 @@ const getPosts = async (req, res) => {
 
   //Get Landing Page Headlines
   if (category === 'topHeadlines') {
-    const inCache = await client.get(category);
-    //set category to expiring redis key
-    if (!inCache) {
-      await client.set(category, 'true');
+    //if no cache set one for 1 hour
+
+    if (!cache && skip == 0) {
+      //Get top new articles from gnews and set in cache
+      const response = await getTopNewsArticles();
+      await updateDB(category, response);
+      const dbResponse = await fetchDB(category, skip, limit);
+      await client.set(category, JSON.stringify(dbResponse));
       //expire in 1 hour
       await client.expire(category, 60 * 60 * 1);
     }
-    //If no skip set then fetch fresh articles from news api
-    if (skip === 0 && !inCache) {
-      const response = await getTopNewsArticles();
-      await updateDB(category, response);
+    //If skip isnt set and cache is populated
+    if (skip === 0 && cache) {
+      let cachedArticles = await client.get(category);
+      res.send(JSON.parse(cachedArticles));
+      return;
     }
 
     const dbResponse = await fetchDB(category, skip, limit);
     res.send(dbResponse);
+
     return;
   } else {
     //Get Category Specific Articles
-    const inCache = await client.get(category);
 
-    if (!inCache) {
-      await client.set(category, 'true');
+    if (!cache && skip == 0) {
+      const response = await getArticlesByCategory(category);
+      await updateDB(category, response);
+      const dbResponse = await fetchDB(category, skip, limit);
+      await client.set(category, JSON.stringify(dbResponse));
       //expire in 2 hours
       await client.expire(category, 60 * 60 * 2);
     }
     //If no skip set then fetch fresh articles from news api
-    if (skip === 0 && !inCache) {
-      const response = await getArticlesByCategory(category);
-      await updateDB(category, response);
+    if (skip === 0 && cache) {
+      let cachedArticles = await client.get(category);
+      res.send(JSON.parse(cachedArticles));
+      return;
     }
+
     const dbResponse = await fetchDB(category, skip, limit);
     res.send(dbResponse);
   }
